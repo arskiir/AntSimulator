e5586e6b2432e1e10b30bdc90057918d6aa50ee8
Sukrit Banthao <69001162+arskiir@users.noreply.github.com>
2021-05-15 12:21:13 +0700

    addAnts only adds ants, nothing else

--- src/gui/SimulationArea.java
+++ src/gui/SimulationArea.java
@@ -10,12 +10,12 @@ import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-import utils.Vector;
+import utils.math.Vector;
 
 public class SimulationArea extends Pane {
 
 	private ImageView houseImage = new ImageView("house.png");
-	private static ArrayList<Ant> ants = new ArrayList<>();
+	public static ArrayList<Ant> ants = new ArrayList<>();
 	private ArrayList<Food> foods = new ArrayList<>();
 	private ArrayList<String> foodImgPaths = new ArrayList<>() {
 		/**
@@ -33,8 +33,9 @@ public class SimulationArea extends Pane {
 
 	private static final int houseImageWidth = 80;
 	private static final int width = Global.WIDTH;
-	private static final int height = 850;
-	private static final Vector origin = new Vector((double) -width / 2, (double) -height / 2, 0); // in conventional x-y plane
+	public static final int height = 850;
+	private static final Vector origin = new Vector((double) width / 2, (double) -height / 2, 0); // in conventional x-y
+																									// plane
 
 	private int dragCount = 0; // if this is a multiple of a number, then add the food, to slow down
 	private static final int dragMultiple = 4;
@@ -68,15 +69,22 @@ public class SimulationArea extends Pane {
 		final String foodPath = this.foodImgPaths.get(this.createdFoodCount % this.foodImgPaths.size());
 		final Food food = new Food(foodPath, position);
 		this.foods.add(food);
-		this.getChildren().add(food.getImg());
+		this.addImage(food.getImg());
 	}
 
-	public static void addAnts(final int numberOfAnts) {
-		new Thread(() -> {
-			for (int ant = 0; ant < numberOfAnts; ++ant) {
-				ants.add(new Ant(origin));
-			}
-		}).start();
+	public void addAnts(final int numberOfAnts) {
+		for (int antCount = 0; antCount < numberOfAnts; ++antCount) {
+			final Ant ant = new Ant(origin);
+			ants.add(ant);
+		}
+	}
+
+	public void removeImage(ImageView img) {
+		this.getChildren().remove(img);
+	}
+
+	public void addImage(ImageView img) {
+		this.getChildren().add(img);
 	}
 
 }
