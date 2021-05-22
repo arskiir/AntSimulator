e2df0f1461465b1f92236519a308484c0c7ff79b
Sukrit Banthao <69001162+arskiir@users.noreply.github.com>
2021-05-22 19:15:18 +0700

    javadoc for PoisonousCandy

--- src/entity/derived/PoisonousCandy.java
+++ src/entity/derived/PoisonousCandy.java
@@ -9,12 +9,28 @@ import gui.SimulationArea;
 import javafx.application.Platform;
 import utils.Vector;
 
+/**
+ * The Class PoisonousCandy. A deadly type of candy that kills the ant that
+ * grabs it.
+ */
 public final class PoisonousCandy extends Candy implements Poisonable {
 
+	/**
+	 * Instantiates a new poisonous candy.
+	 *
+	 * @param imgPath  the path to the actual image
+	 * @param position the position vector
+	 */
 	public PoisonousCandy(String imgPath, Vector position) {
 		super(imgPath, position);
 	}
 
+	/**
+	 * Kills the ant completely, consisting of different steps including removing
+	 * the ant, removing itself, and update the control bar.
+	 *
+	 * @param ant the ant
+	 */
 	@Override
 	public void poison(Ant ant) {
 		// kill the ant
@@ -22,22 +38,35 @@ public final class PoisonousCandy extends Candy implements Poisonable {
 		this.disappear();
 		this.updateControlBar(ant);
 	}
-	
+
+	/**
+	 * Removes the ant.
+	 *
+	 * @param ant the ant
+	 */
 	private void removeAnt(Ant ant) {
 		ant.playOutroSound();
 		ant.getSettingOffJourneyThread().interrupt();
 		Main.getSimulationArea().getAnts().remove(ant);
 	}
 
+	/**
+	 * Removes itself
+	 */
 	private void disappear() {
 		// remove this food from the area
-		final SimulationArea simulationArea = Main.getSimulationArea();
+		final var simulationArea = Main.getSimulationArea();
 		Platform.runLater(() -> simulationArea.getChildren().remove(this.img));
 		simulationArea.getFoods().remove(this);
 	}
 
+	/**
+	 * Update control bar.
+	 *
+	 * @param ant the poor ant
+	 */
 	private void updateControlBar(Ant ant) {
-		final ControlBar controlBar = Main.getControlBar();
+		final var controlBar = Main.getControlBar();
 		controlBar.setPopulation(controlBar.getPopulation() - 1);
 		controlBar.setDeadCount(controlBar.getDeadCount() + 1);
 		if (ant instanceof FlashAnt)
