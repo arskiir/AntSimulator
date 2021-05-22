aee90976ae06aed9a738b4a3f229414cf4ef948c
Sukrit Banthao <69001162+arskiir@users.noreply.github.com>
2021-05-22 19:56:58 +0700

    first javadoc draft for SumilationArea

--- src/gui/SimulationArea.java
+++ src/gui/SimulationArea.java
@@ -18,12 +18,21 @@ import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import utils.Vector;
 
+/**
+ * The Class SimulationArea. An area where all actions happen.
+ */
 public class SimulationArea extends Pane {
 
+	/** The ants. */
 	private ArrayList<Ant> ants = new ArrayList<>();
+	
+	/** The foods. */
 	private CopyOnWriteArrayList<Candy> foods = new CopyOnWriteArrayList<>();
 	
+	/** The house image. */
 	private ImageView houseImage = new ImageView("house.png");
+	
+	/** The food img paths. */
 	private ArrayList<String> foodImgPaths = new ArrayList<>() {
 		private static final long serialVersionUID = -3891700759148049065L;
 		{
@@ -32,26 +41,46 @@ public class SimulationArea extends Pane {
 			add("food3.png");
 		}
 	};
+	
+	/** The created food count. */
 	private int createdFoodCount = 0;
 
+	/** The Constant maxPopulation. */
 	public static final int maxPopulation = 100;
+	
+	/** The has won. */
 	private boolean hasWon = false;
 
+	/** The Constant houseImageWidth. */
 	private static final int houseImageWidth = 50;
+	
+	/** The Constant height. */
 	public static final int height = 800;
+	
+	/** The Constant origin. */
 	public static final Vector origin = new Vector((double) Global.WIDTH / 2, (double) -height / 2, 0);
 
+	/** The drag count. */
 	private int dragCount = 0; // if this is a multiple of a number, then add the food, to slow down
+	
+	/** The Constant dragMultiple. */
 	private static final int dragMultiple = 4;
 
+	/** The Constant random. */
 	public static final Random random = new Random();
 
+	/**
+	 * Instantiates a new simulation area.
+	 */
 	public SimulationArea() {
 		super();
 		this.setupUI();
 		this.setupCreateFoodEventListeners();
 	}
 
+	/**
+	 * Setup create food event listeners.
+	 */
 	private void setupCreateFoodEventListeners() {
 		this.setOnMouseDragged(e -> {
 			this.dragCount++;
@@ -73,6 +102,9 @@ public class SimulationArea extends Pane {
 		});
 	}
 
+	/**
+	 * Setup UI.
+	 */
 	private void setupUI() {
 		this.setMinHeight(height);
 		this.setPrefWidth(Global.WIDTH);
@@ -86,14 +118,29 @@ public class SimulationArea extends Pane {
 
 	}
 
+	/**
+	 * Checks if is checks for won.
+	 *
+	 * @return true, if is checks for won
+	 */
 	public boolean isHasWon() {
 		return hasWon;
 	}
 
+	/**
+	 * Sets the checks for won.
+	 *
+	 * @param hasWon the new checks for won
+	 */
 	public void setHasWon(boolean hasWon) {
 		this.hasWon = hasWon;
 	}
 
+	/**
+	 * Creates the food.
+	 *
+	 * @param position the position
+	 */
 	public void createFood(Vector position) {
 		// create new Food object and add to container to keep track
 		// also show it in the area
@@ -116,6 +163,11 @@ public class SimulationArea extends Pane {
 		this.addImage(foodImage);
 	}
 
+	/**
+	 * Adds the ant.
+	 *
+	 * @param type the type
+	 */
 	public void addAnt(final AntType type) {
 		final ControlBar controlBar = Main.getControlBar();
 
@@ -141,24 +193,43 @@ public class SimulationArea extends Pane {
 		ant.getSettingOffJourneyThread().start();
 	}
 
+	/**
+	 * Reset foods.
+	 */
 	public void resetFoods() {
 		this.foods.forEach(food -> food.getImg().setVisible(false));
 		this.foods.clear();
 	}
 
+	/**
+	 * Reset ants.
+	 */
 	private void resetAnts() {
 		this.ants.forEach(ant -> ant.getSettingOffJourneyThread().interrupt());
 		this.ants.clear();
 	}
 
+	/**
+	 * Removes the image.
+	 *
+	 * @param img the img
+	 */
 	public void removeImage(ImageView img) {
 		this.getChildren().remove(img);
 	}
 
+	/**
+	 * Adds the image.
+	 *
+	 * @param img the img
+	 */
 	public void addImage(ImageView img) {
 		this.getChildren().add(img);
 	}
 
+	/**
+	 * Reset.
+	 */
 	public void reset() {
 		this.getChildren().clear();
 		resetFoods();
@@ -167,14 +238,29 @@ public class SimulationArea extends Pane {
 		setupUI();
 	}
 
+	/**
+	 * Gets the ants.
+	 *
+	 * @return the ants
+	 */
 	public List<Ant> getAnts() {
 		return this.ants;
 	}
 
+	/**
+	 * Gets the foods.
+	 *
+	 * @return the foods
+	 */
 	public List<Candy> getFoods() {
 		return foods;
 	}
 
+	/**
+	 * Sets the foods.
+	 *
+	 * @param foods the new foods
+	 */
 	public void setFoods(List<Candy> foods) {
 		this.foods = (CopyOnWriteArrayList<Candy>) foods;
 	}
