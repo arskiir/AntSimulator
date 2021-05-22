b138a3489f2383232905e5d8235389b7c526d061
Sukrit Banthao <69001162+arskiir@users.noreply.github.com>
2021-05-22 19:32:38 +0700

    javadoc for Global

--- src/gui/Global.java
+++ src/gui/Global.java
@@ -1,9 +1,16 @@
 package gui;
 
+/**
+ * The Class Global containing shared values.
+ */
 public final class Global {
 
+	/** The width of the application and all panes */
 	public static final int WIDTH = 1500; // pixel
 
+	/**
+	 * Ignore this.
+	 */
 	private Global() {
 	}
 }
