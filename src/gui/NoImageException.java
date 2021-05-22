5898cd78bfa5a54c77c9c1341382ae30f210bd6d
Sukrit Banthao <69001162+arskiir@users.noreply.github.com>
2021-05-22 19:34:30 +0700

    javadoc for this

--- src/gui/NoImageException.java
+++ src/gui/NoImageException.java
@@ -1,8 +1,19 @@
 package gui;
 
+/**
+ * The Class NoImageException. Used to throw when a method relating to
+ * Renderable interface is called inappropriately
+ */
 public class NoImageException extends Exception {
+
+	/** The Constant serialVersionUID. */
 	private static final long serialVersionUID = -7511655467882506948L;
-	
+
+	/**
+	 * Instantiates a new no image exception.
+	 *
+	 * @param message the message
+	 */
 	public NoImageException(String message) {
 		super(message);
 	}
