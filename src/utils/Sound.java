ad25391322d77729c858e2158bdd5f173cb359d6
Sukrit Banthao <69001162+arskiir@users.noreply.github.com>
2021-05-22 22:00:47 +0700

    added javadoc

--- src/utils/Sound.java
+++ src/utils/Sound.java
@@ -5,22 +5,37 @@ import java.io.File;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
+/**
+ * The Class Sound. Provides useful methods when working with sounds
+ */
 public class Sound {
+	
+	/**
+	 * Instantiates a new sound.
+	 */
 	private Sound() {
 	}
 
 	/**
-	 * get MediaPlayer instance of a media file and volume within [0.0 1.0] where
+	 * Gets MediaPlayer instance of a media file and volume within [0.0 1.0] where
 	 * 0.0 is inaudible and 1.0 is full volume, which is the default
+	 *
+	 * @param src the path to the file
+	 * @param volume the volume
+	 * @return the media player
 	 */
 	public static MediaPlayer getMediaPlayer(String src, double volume) {
-		Media sound = new Media(new File(src).toURI().toString());
-		MediaPlayer player = new MediaPlayer(sound);
+		var sound = new Media(new File(src).toURI().toString());
+		var player = new MediaPlayer(sound);
 		player.setVolume(volume);
 		return player;
 	}
 	
-	/** can play sound repeatedly */
+	/**
+	 * Plays the player from the start
+	 *
+	 * @param player the player
+	 */
 	public static void play(MediaPlayer player) {
 		player.seek(player.getStartTime());
 		player.play();
--- src/utils/Vector.java
+++ src/utils/Vector.java
@@ -1,9 +1,17 @@
 package utils;
 
+/**
+ * The Class Vector. Simple mathematical vector class.
+ */
 public class Vector {
 
+	/** The x component. */
 	double x;
+	
+	/** The y component. */
 	double y;
+	
+	/** The z component. */
 	double z;
 
 	/** Constructor to create a null vector i.e. 0i+0j+0k */
@@ -13,14 +21,24 @@ public class Vector {
 		this.z = 0;
 	}
 
-	/** Constructor to create a vector with the given values of i,j and k */
+	/**
+	 *  Constructor to create a vector with the given values of i,j and k.
+	 *
+	 * @param x the x component
+	 * @param y the y component
+	 * @param z the z component
+	 */
 	public Vector(double x, double y, double z) {
 		this.x = x;
 		this.y = y;
 		this.z = z;
 	}
 
-	/** Copy constructor */
+	/**
+	 *  Copy constructor.
+	 *
+	 * @param v the vector to be copied
+	 */
 	public Vector(Vector v) {
 		this.x = v.x;
 		this.y = v.y;
@@ -28,25 +46,42 @@ public class Vector {
 	}
 
 	/**
-	 * Constructor to create a 2D vector with the given angle in degree and length
+	 * Constructor to create a 2D vector with the given angle in degree and length.
+	 *
+	 * @param angle the angle in degree
+	 * @param length the length of the vector
+	 * @return the vector
 	 */
 	public static Vector createVector2FromAngle(double angle, double length) {
 		return new Vector(Math.cos(angle * Math.PI / 180) * length, Math.sin(angle * Math.PI / 180) * length, 0);
 	}
 
+	/**
+	 * Reverse Y.
+	 */
 	public void reverseY() {
 		this.y = this.y * -1;
 	}
 
+	/**
+	 * Reverse X.
+	 */
 	public void reverseX() {
 		this.x = this.x * -1;
 	}
 
+	/**
+	 * Reverse Z.
+	 */
 	public void reverseZ() {
 		this.z = this.z * -1;
 	}
 
-	/** Return the string notation of the Vector */
+	/**
+	 *  Return the string notation of the Vector.
+	 *
+	 * @return the complete string notation
+	 */
 	public String getCompleteStringNotation() {
 		return this.x + "i + " + this.y + "j + " + this.z + "k";
 	}
@@ -54,9 +89,11 @@ public class Vector {
 	/**
 	 * Return the string notation of the Vector.Reduced it if any of x,y and z are
 	 * integers
+	 *
+	 * @return the reduced string notation
 	 */
 	public String getReducedStringNotation() {
-		String not = "";
+		var not = "";
 		if (this.x != 0) {
 			if (this.x == (int) this.x)
 				not = not + (int) this.x + "i^+";
@@ -86,12 +123,21 @@ public class Vector {
 		return not;
 	}
 
-	/** Gives the length of the vector */
+	/**
+	 *  Gives the length of the vector.
+	 *
+	 * @return the length of the vector
+	 */
 	public double modulus() {
 		return Math.sqrt(Math.hypot(x, Math.hypot(y, z)));
 	}
 
-	/** Check if two vectors are parallel or not */
+	/**
+	 *  Check if two vectors are parallel or not.
+	 *
+	 * @param v the vector to be compared
+	 * @return true, if is parallel
+	 */
 	public boolean isParallel(Vector v) {
 		double xr = this.x / v.x;
 		double yr = this.y / v.y;
@@ -99,18 +145,32 @@ public class Vector {
 		return (xr == yr && yr == zr && zr == xr);
 	}
 
-	/** Returns the unit vector in the direction of the Calling vector object */
+	/**
+	 *  Returns the unit vector in the direction of the Calling vector object.
+	 *
+	 * @return the unit vector
+	 */
 	public Vector getUnitVector() {
 		double mod = this.modulus();
 		return new Vector(this.x / mod, this.y / mod, this.z / mod);
 	}
 
-	/** Computes and return the dot product between two vector */
+	/**
+	 *  Computes and return the dot product between two vector.
+	 *
+	 * @param v the v
+	 * @return the double
+	 */
 	public double dotProduct(Vector v) {
 		return this.x * v.x + this.y * v.y + this.z * v.z;
 	}
 
-	/** Compute and returns the angle between two vectors in radians */
+	/**
+	 *  Compute and returns the angle between two vectors in radians.
+	 *
+	 * @param v the vector to be compared with
+	 * @return the angle between two vectors in radians
+	 */
 	public double angleBetween(Vector v) {
 		double dotPro = this.dotProduct(v);
 		double modObj = this.modulus();
@@ -118,7 +178,12 @@ public class Vector {
 		return Math.acos(dotPro / (modObj * modV));
 	}
 
-	/** Computes and returns the cross product of the two vector */
+	/**
+	 *  Computes and returns the cross product of the two vector.
+	 *
+	 * @param v the vector to be computed with
+	 * @return the vector from the cross product
+	 */
 	public Vector crossProduct(Vector v) {
 		double resx = (this.y * v.z) - (v.y * this.z);
 		double resy = -((this.x * v.z) - (v.x * this.z));
@@ -126,31 +191,65 @@ public class Vector {
 		return new Vector(resx, resy, resz);
 	}
 
+	/**
+	 * Gets the x.
+	 *
+	 * @return the x
+	 */
 	public double getX() {
 		return x;
 	}
 
+	/**
+	 * Gets the y.
+	 *
+	 * @return the y component
+	 */
 	public double getY() {
 		return y;
 	}
 
+	/**
+	 * Gets the z.
+	 *
+	 * @return the z component
+	 */
 	public double getZ() {
 		return z;
 	}
 
+	/**
+	 * Sets the x.
+	 *
+	 * @param x the new x component
+	 */
 	public void setX(double x) {
 		this.x = x;
 	}
 
+	/**
+	 * Sets the y.
+	 *
+	 * @param y the new y component
+	 */
 	public void setY(double y) {
 		this.y = y;
 	}
 
+	/**
+	 * Sets the z.
+	 *
+	 * @param z the new z component
+	 */
 	public void setZ(double z) {
 		this.z = z;
 	}
 
-	/** returns the angle in degree between x and y */
+	/**
+	 *  returns the angle in degree between x and y.
+	 *
+	 * @return the angle in degree
+	 */
 	public double getAngle() {
 		final double angle = Math.atan(this.y / this.x) * 180 / Math.PI;
 		if ((this.x < 0 && this.y > 0) || (this.x < 0 && this.y < 0)) {
