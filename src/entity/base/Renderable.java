2cbc99f9d95ca408976a0960e9ceaa250498433f
Sukrit Banthao <69001162+arskiir@users.noreply.github.com>
2021-05-22 19:06:05 +0700

    javadoc for Renderable interface

--- src/entity/base/Renderable.java
+++ src/entity/base/Renderable.java
@@ -3,8 +3,29 @@ package entity.base;
 import gui.NoImageException;
 import javafx.scene.image.ImageView;
 
+/**
+ * The Interface Renderable. Any class that implements this must be able to render to the pane.
+ */
 public interface Renderable {
+	
+	/**
+	 * Renders to the pane.
+	 */
 	public void rerender();
+	
+	/**
+	 * Gets the image.
+	 *
+	 * @return the image
+	 * @throws NoImageException the no image exception
+	 */
 	public ImageView getImg() throws NoImageException;
+	
+	/**
+	 * Sets the image.
+	 *
+	 * @param img the new image
+	 * @throws NoImageException the no image exception
+	 */
 	public void setImg(ImageView img) throws NoImageException;
 }
