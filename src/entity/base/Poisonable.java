5fa958f0910857ce4f2fc2fe4cb1d140afa662ad
Sukrit Banthao <69001162+arskiir@users.noreply.github.com>
2021-05-22 19:04:08 +0700

    javadoc for Poisonable interface

--- src/entity/base/Poisonable.java
+++ src/entity/base/Poisonable.java
@@ -1,5 +1,14 @@
 package entity.base;
 
+/**
+ * The Interface Poisonable.
+ */
 public interface Poisonable {
+	
+	/**
+	 * Poisons the ant that grabs this poisonous candy.
+	 *
+	 * @param ant the poor ant
+	 */
 	public void poison(Ant ant);
 }
